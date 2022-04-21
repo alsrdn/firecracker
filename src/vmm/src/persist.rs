4bc575e2f295447b3e9ef0e9c352e0a559f1da68
Alexandru-Cezar Sardan <alsardan@amazon.com>
2022-04-21 14:21:22 +0000

    snaps/x86: allow restore on different CPU vendor
    We were not allowing restore from a snapshot where the host CPU vendor
    was different from the one saved in the state.
    Sometimes x86_64 CPUs are compatible and can run the restore microVM
    without issues even if they have different vendor ids.
    Relax this requirement for now since it's too coarse grained and
    restrictive. We'll add some more fine grained sanity checks for checking
    CPU compatibility.
    
    Signed-off-by: Alexandru-Cezar Sardan <alsardan@amazon.com>

--- src/vmm/src/persist.rs
+++ src/vmm/src/persist.rs
@@ -425,8 +425,6 @@ pub fn snapshot_state_sanity_check(
         ));
     }
 
-    #[cfg(target_arch = "x86_64")]
-    validate_cpu_vendor(&microvm_state)?;
     #[cfg(target_arch = "aarch64")]
     validate_cpu_manufacturer_id(&microvm_state)?;
 
